11ad78abb1d6d6a08af33be07bc464be0aedf095
AdamChrimes <iChrimes@live.co.uk>
2020-06-15 17:01:46 +0100

    add home and delivery address journey

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,16 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Version 1 - Is your delivery address the same as your home address?
+router.post('/v1/action/delivery-address-question', function (req, res) {
+  var deliveryAddressAnswer = req.session.data['delivery-address']
+
+  if (deliveryAddressAnswer == "Yes"){
+    res.redirect('/v1/home-test-kit/email-address')
+  } else {
+    res.redirect('/v1/home-test-kit/delivery-address')
+  }
+
+})
+
 module.exports = router
