8232116416bf831bdad908a7cba50f605a4b69ae
AdamChrimes <iChrimes@live.co.uk>
2020-06-15 18:22:53 +0100

    add address lookup results

--- app/routes.js
+++ app/routes.js
@@ -15,4 +15,16 @@ router.post('/v1/action/delivery-address-question', function (req, res) {
 
 })
 
+// Version 1 - Confirm identity
+router.post('/v1/action/confirm-identity', function (req, res) {
+  var deliveryAddressAnswer = req.session.data['identity']
+
+  if (deliveryAddressAnswer == "Yes"){
+    res.redirect('/v1/home-test-kit/order-summary')
+  } else {
+    res.redirect('/v1/home-test-kit/')
+  }
+
+})
+
 module.exports = router
