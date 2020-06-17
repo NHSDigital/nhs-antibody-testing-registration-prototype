f305a1b98ca1e86e39f4bfe4434578b68063fb2a
AdamChrimes <iChrimes@live.co.uk>
2020-06-17 16:32:28 +0100

    pass mobile number data from one route to another

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
-
 // Version 1 - Is your delivery address the same as your home address?
 router.post('/v1/action/delivery-address-question', function (req, res) {
   var deliveryAddressAnswer = req.session.data['delivery-address']
@@ -27,11 +25,18 @@ router.post('/v1/action/confirm-identity', function (req, res) {
 
 })
 
-// Version 1 - Registration - Email address
+// Version 1 - Registration - Get phone number for theeEmail address route
+let mobilePhoneNumber = "";
+router.post('/v1/action/mobile-number', function (req, res) {
+  mobilePhoneNumber = req.session.data['mobile-number'];
+  res.redirect('/v1/registration/email-address');
+
+})
+
+
+// Version 1 - Registration - Email address route
 router.post('/v1/action/email-address', function (req, res) {
   var emailAddress = req.session.data['email']
-  var mobilePhoneNumber = req.session.data['mobile-number']
-  console.log(emailAddress)
 
   if (emailAddress == "No" && mobilePhoneNumber == "No"){
     res.redirect('/v1/registration/call-us')
