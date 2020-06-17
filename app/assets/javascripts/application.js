7e6920898c0b454606c0bb2734b6ab2fd3214c0c
AdamChrimes <iChrimes@live.co.uk>
2020-06-17 11:39:33 +0100

    tidy up form markup for registration process

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,23 +8,3 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
-
-// Check if radio button has been selected
-const radioButton = $("input[value$='Yes']")
-if(!radioButton.attr('checked')) {
-  $("div.input-box").hide()
-}
-
-// toggle input div based on selected option
-$(document).ready(function() {
-  $("input[type$='radio']").click(function() {
-    
-    const buttonValue = $(this).val();
-      if(buttonValue === "No") {
-        $("div.input-box").hide()
-      } else {
-        $("div.input-box").show()
-      }
-      
-  });
-});
