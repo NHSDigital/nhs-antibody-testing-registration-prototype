d2d076d5cd01d0d1298c47fa6c580df1c451cd53
Matt Fielding <matt@hippodigital.co.uk>
2020-06-19 02:03:50 +0100

    rename section to match diagram

--- app/routes.js
+++ app/routes.js
@@ -8,21 +8,9 @@ router.post('/v1/action/delivery-address-question', function (req, res) {
   var deliveryAddressAnswer = req.session.data['delivery-address-same']
 
   if (deliveryAddressAnswer == "Yes"){
-    res.redirect('/v1/home-test-kit/order-summary')
+    res.redirect('/v1/order/order-summary')
   } else {
-    res.redirect('/v1/home-test-kit/delivery-address')
-  }
-
-})
-
-// Version 1 - Confirm identity
-router.post('/v1/action/confirm-identity', function (req, res) {
-  var deliveryAddressAnswer = req.session.data['identity']
-
-  if (deliveryAddressAnswer == "Yes"){
-    res.redirect('/v1/home-test-kit/order-summary')
-  } else {
-    res.redirect('/v1/home-test-kit/')
+    res.redirect('/v1/order/delivery-address')
   }
 
 })
