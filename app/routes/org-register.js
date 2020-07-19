6beb4327989da19aa6121063a931331c1aaca6cb
grillopress <grillopress@gmail.com>
2020-07-19 20:12:13 +0100

    add v1 bulk and one by one journey

--- app/routes/org-register.js
+++ app/routes/org-register.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+// v0 routes
+
 router.post('/organisational/register/v0/knowNHSnumber', function (req, res) {
   let answer = req.body.cuKnowNHSnumber;
 
@@ -9,6 +11,33 @@ router.post('/organisational/register/v0/knowNHSnumber', function (req, res) {
     } else {
     res.redirect('/organisational/register/v0/check')
     }
-  });
+});
+
+// v1 routes
+
+router.post('/organisational/register/v1/how', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/organisational/register/v1/bulk/staff')
+    } else if (answer == 'single') {
+      res.redirect('/organisational/register/v1/single/staff')
+    } else {
+    res.redirect('/organisational/register/v1/how?error=empty')
+    }
+});
+
+router.post('/organisational/register/v1/single/knowNHSnumber', function (req, res) {
+  let answer = req.body.cuKnowNHSnumber;
+
+  if (answer == 'yes') {
+    res.redirect('/organisational/register/v1/single/nhsnumber')
+    } else {
+    res.redirect('/organisational/register/v1/single/testkit')
+    }
+});
+
+
+
 
 module.exports = router
