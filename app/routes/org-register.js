610b23ccca208105c68c0fcb2f1898d050efb5c7
grillopress <grillopress@gmail.com>
2020-07-20 08:35:06 +0100

    add v2 journey

--- app/routes/org-register.js
+++ app/routes/org-register.js
@@ -38,6 +38,31 @@ router.post('/organisational/register/v1/single/knowNHSnumber', function (req, r
 });
 
 
+// v2 routes
+
+router.post('/organisational/register/v2/how', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/organisational/register/v2/bulk/staff')
+    } else if (answer == 'single') {
+      res.redirect('/organisational/register/v2/single/staff')
+    } else {
+    res.redirect('/organisational/register/v2/how?error=empty')
+    }
+});
+
+router.post('/organisational/register/v2/single/knowNHSnumber', function (req, res) {
+  let answer = req.body.cuKnowNHSnumber;
+
+  if (answer == 'yes') {
+    res.redirect('/organisational/register/v2/single/nhsnumber')
+    } else {
+    res.redirect('/organisational/register/v2/single/testkit')
+    }
+});
+
+
 
 
 module.exports = router
