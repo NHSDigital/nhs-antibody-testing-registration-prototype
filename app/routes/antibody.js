b4a0b45fea5d63e27b3aa1bd5103cc06319c32ef
AmeyaKrishnamoorthy <ameya@hippodigital.co.uk>
2020-08-17 00:56:15 +0200

    Update antibody.js

--- app/routes/antibody.js
+++ app/routes/antibody.js
@@ -349,11 +349,11 @@ router.post('/antibody/v4/action/landline-number', function (req, res) {
   let country = req.session.data['country']
 
   if (country == "England"){
-    res.redirect('/antibody/v4/refer-and-triage/nhs-number-known')
+    res.redirect('/antibody/v4/global-registration/nhs-number-known')
   } else if (country = "Scotland") {
-    res.redirect('/antibody/v4/refer-and-triage/chi-number-known')
+    res.redirect('/antibody/v4/global-registration/chi-number-known')
   } else if (country = "Northern Ireland") {
-    res.redirect('/antibody/v4/refer-and-triage/h&c-number-known')
+    res.redirect('/antibody/v4/global-registration/h&c-number-known')
   }
 })
 
