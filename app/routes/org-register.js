6b047391c7016639f1480df192c846e1354cee66
grillopress <grillopress@gmail.com>
2020-09-01 21:23:27 +0100

    add v3 and v3 routes

--- app/routes/org-register.js
+++ app/routes/org-register.js
@@ -101,6 +101,68 @@ router.post('/organisational/register/v2/single/ethnic-group', function (req, re
 });
 
 
+// v3 routes
+
+router.post('/organisational/register/v3/how', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/organisational/register/v3/bulk/staff')
+    } else if (answer == 'single') {
+      res.redirect('/organisational/register/v3/single/staff')
+    } else {
+    res.redirect('/organisational/register/v3/how?error=empty')
+    }
+});
+
+router.post('/organisational/register/v3/single/knowNHSnumber', function (req, res) {
+  let answer = req.body.cuKnowNHSnumber;
+
+  if (answer == 'yes') {
+    res.redirect('/organisational/register/v3/single/nhsnumber')
+    } else {
+    res.redirect('/organisational/register/v3/single/have-coronavirus')
+    }
+});
+
+router.post('/organisational/register/v3/single/have-coronavirus', function (req, res) {
+  let answer = req.body.cuCoronavirus;
+
+  if (answer == 'Yes') {
+    res.redirect('/organisational/register/v3/single/when-symptoms')
+    } else {
+    res.redirect('/organisational/register/v3/single/testkit')
+    }
+});
+
+router.post('/organisational/register/v3/single/occupation/index', function (req, res) {
+  let answer = req.body.cuInWork;
+
+  if (answer == "Yes, and for the last 2 weeks they've worked from home") {
+    res.redirect('/organisational/register/v3/single/occupation/area')
+  } else if (answer == "Yes, and for the last 2 weeks they've travelled to work") {
+    res.redirect('/organisational/register/v3/single/occupation/area')
+  } else if (answer == 'no') {
+    res.redirect('/organisational/register/v3/single/country')
+  } else if (answer == 'Prefer not to say') {
+    res.redirect('/organisational/register/v3/single/country')
+  } else {
+    res.redirect('/organisational/register/v3/single/occupation/index?error=empty')
+    }
+});
+
+router.post('/organisational/register/v3/single/ethnic-group', function (req, res) {
+  let answer = req.body.cuEthnicGroup;
+
+  if (answer == 'Prefer not to say') {
+    res.redirect('/organisational/register/v3/single/occupation/index')
+    } else if (answer) {
+      res.redirect('/organisational/register/v3/single/ethnic-desc')
+    } else {
+    res.redirect('/organisational/register/v3/single/ethnic-group?error=empty')
+    }
+});
+
 
 // Antibody Care home routes
 
