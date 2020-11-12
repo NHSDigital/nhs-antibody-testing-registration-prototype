e60c311f3e78fb25bef2e466e5cc33a3f5986cbb
Andrea Zavodska <azavodska@deloitte.co.uk>
2020-11-12 13:13:00 +0000

    fix route for people confirmed person 1

--- app/routes/antigen.js
+++ app/routes/antigen.js
@@ -227,6 +227,29 @@ router.post('/antigen/v2/action3/people-confirmed', function (req, res) {
 
 })
 
+router.post('/antigen/v2/action3/people-confirmed-person-1', function (req, res) {
+  let postcode = req.session.data['home-postcode']
+  let emailAddress = req.session.data['email']
+  let car = req.session.data['do-you-have-a-car']
+  let chosenWayToTest = req.session.data['way-to-test']
+  if (chosenWayToTest == "drive-through"){
+    res.redirect('/antigen/v2/site-appointment-booking/find-test-site')
+  } else if (chosenWayToTest == "walk-in") {
+    res.redirect('/antigen/v2/site-appointment-booking/find-test-site')
+  } else if (chosenWayToTest == "home testing") {
+    res.redirect('/antigen/v2/order-home-test-kit/')
+  } else if (car == "No" && postcode == "N0000" && emailAddress == "Yes") {
+    res.redirect('/antigen/v2/order-home-test-kit/')
+  } else if (car == "Yes" && postcode == "N0000" && emailAddress !== "Yes") {
+    res.redirect('/antigen/v2/site-appointment-booking/find-test-site')
+  } else if (car == "No" && postcode !== "N0000" && emailAddress !== "Yes") {
+    res.redirect('/antigen/v2/site-appointment-booking/find-test-site')
+  } else {
+    res.redirect('/antigen/v2/site-appointment-booking/find-test-site')
+  }
+
+})
+
 // Version 2 - Antigen Site Appointment Booking - find a test site route
 
 router.post('/antigen/v2/action3/find-test-site', function (req, res) {
