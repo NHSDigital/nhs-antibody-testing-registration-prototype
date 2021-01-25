5c851ef4a2bb55b5de413aa34a54e6b1ffdf4148
Andrea Zavodska <azavodska@deloitte.co.uk>
2021-01-25 17:30:28 +0000

    update copy

--- app/routes/lfd-home-ordering.js
+++ app/routes/lfd-home-ordering.js
@@ -10,7 +10,7 @@ function loadJSONFromFile(fileName, path = "app/data/") {
   return JSON.parse(jsonFile) // Return JSON as object
 }
 
-// Version 1 - Daily Contact Testing - do you have symptoms route
+// Version 1 - LFD home ordering
 
 router.post('/lfd-home-ordering/v1/action10/do-you-have-symptoms', function (req, res) {
     let symptoms = req.session.data['do-you-have-symptoms']
@@ -89,6 +89,98 @@ router.post('/lfd-home-ordering/v1/action10/home-address-question', function (re
     } else {
         res.redirect('/lfd-home-ordering/v1/order-home-test-kit/confirm-email-address')
     }
+
+})
+
+// Version 2 - LFD home ordering
+
+router.post('/lfd-home-ordering/v2/action10/do-you-have-symptoms', function (req, res) {
+  let symptoms = req.session.data['do-you-have-symptoms']
+  if (symptoms == "No" ) {
+    res.redirect('/lfd-home-ordering/v2/country')
+  } else {
+    res.redirect('/lfd-home-ordering/v2/different-test')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/country', function (req, res) {
+  let country = req.session.data['country']
+  if (country == "England" ) {
+    res.redirect('/lfd-home-ordering/v2/contact-with-positive')
+  } else {
+    res.redirect('/lfd-home-ordering/v2/country-test-unavailable')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/contact-with-positive', function (req, res) {
+  let contactWithPositive = req.session.data['contact-with-positive']
+  if (contactWithPositive == "No" ) {
+    res.redirect('/lfd-home-ordering/v2/work-from-home')
+  } else {
+    res.redirect('/lfd-home-ordering/v2/notified-how')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/work-from-home', function (req, res) {
+  let workFromHome = req.session.data['work-from-home']
+  if (workFromHome == "No" ) {
+    res.redirect('/lfd-home-ordering/v2/workplace-testing')
+  } else {
+    res.redirect('/lfd-home-ordering/v2/lft-unavailable')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/workplace-testing', function (req, res) {
+  let workplaceTesting = req.session.data['workplace-testing']
+  if (workplaceTesting == "No" ) {
+    res.redirect('/lfd-home-ordering/v2/name')
+  } else {
+    res.redirect('/lfd-home-ordering/v2/lft-unavailable')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/email-address', function (req, res) {
+  let emailAddress = req.session.data['email']
+  if (emailAddress == "No" ) {
+      res.redirect('/lfd-home-ordering/v2/call-us')
+  } else {
+      res.redirect('/lfd-home-ordering/v2/mobile-number')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/contact-with-positive', function (req, res) {
+  let contactWithPositive = req.session.data['contact-with-positive']
+  if (contactWithPositive == "No" ) {
+      res.redirect('/lfd-home-ordering/v2/dct-unavailable')
+  } else {
+      res.redirect('/lfd-home-ordering/v2/notified-how')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/dct-opt-in', function (req, res) {
+  let dctOptIn = req.session.data['dct-opt-in']
+  if (dctOptIn == "No" ) {
+      res.redirect('/lfd-home-ordering/v2/self-isolate')
+  } else {
+      res.redirect('/lfd-home-ordering/v2/name')
+  }
+
+})
+
+router.post('/lfd-home-ordering/v2/action10/home-address-question', function (req, res) {
+  let deliveryAddressSame = req.session.data['delivery-address-same']
+  if (deliveryAddressSame == "No" ) {
+      res.redirect('/lfd-home-ordering/v2/order-home-test-kit/delivery-postcode')
+  } else {
+      res.redirect('/lfd-home-ordering/v2/order-home-test-kit/confirm-email-address')
+  }
 
 })
 
