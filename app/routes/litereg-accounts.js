3c88e423854f7d02a09f829968eb3829adaaea9e
Andrea Zavodska <azavodska@deloitte.co.uk>
2021-02-05 17:58:51 +0000

    update copy

--- app/routes/litereg-accounts.js
+++ app/routes/litereg-accounts.js
@@ -438,7 +438,7 @@ router.post('/litereg-accounts/v2/action9/landline-number', function (req, res) 
   // Version 2 - Lite Registration Accounts - test place route
   router.post('/litereg-accounts/v2/action9/test-place', function (req, res) {
     let testPlace = req.session.data['test-place']
-    if (testPlace == "At home or an accomodation of your choice" || testPlace == "At home or an accomodation of their choice") {
+    if (testPlace == "At home or an accommodation of your choice" || testPlace == "At home or an accommodation of their choice") {
       res.redirect('/litereg-accounts/v2/royal-mail-barcode')
     } else {
       res.redirect('/litereg-accounts/v2/site-id')
