a02486069e9f6316382b749890248e21ff768458
Andrea Zavodska <azavodska@deloitte.co.uk>
2021-04-15 17:10:50 +0100

    update copy

--- app/routes.js
+++ app/routes.js
@@ -22,6 +22,7 @@ router.use('/', require('./routes/create-account.js'))
 router.use('/', require('./routes/pre-registration.js'))
 router.use('/', require('./routes/delegated-access-results-recorder.js'))
 router.use('/', require('./routes/lfd-collection-registration.js'))
+router.use('/', require('./routes/international-arrivals.js'))
 
 // Pull scope into the homepage to show/hide sections
 // 'SCOPE' is either pulled in from the Heroku App settings or setting in a local .env file eg. SCOPE=antibody
---
+++ app/routes/international-arrivals.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router()
+const fs = require("fs-extra")
+
+// Load JSON data from file ----------------------------------------------------
+
+// fileName excludes path but includes extension e.g. file.json
+function loadJSONFromFile(fileName, path = "app/data/") {
+  let jsonFile = fs.readFileSync(path + fileName)
+  return JSON.parse(jsonFile) // Return JSON as object
+}
+
+router.post('/international-arrivals/v1/action1/travel-route', function (req, res) {
+    let travelRoute = req.session.data['travel-route']
+    if (travelRoute == "Red") {
+      res.redirect('/international-arrivals/v1/red-package-exit')
+    } else {
+      res.redirect('/international-arrivals/v1/travel-details')
+    }
+})
+
+router.post('/international-arrivals/v1/action2/contact-details-same-person-1', function (req, res) {
+  let contactDetailsSame = req.session.data['contact-details-same-person-1']
+  if (contactDetailsSame == "Yes") {
+    res.redirect('/international-arrivals/v1/travel-route-same-person-1')
+  } else {
+    res.redirect('/international-arrivals/v1/contact-details-person-1')
+  }
+})
+
+router.post('/international-arrivals/v1/action3/travel-route-same-person-1', function (req, res) {
+  let travelRouteSame = req.session.data['travel-route-same-person-1']
+  if (travelRouteSame == "No") {
+    res.redirect('/international-arrivals/v1/cannot-add-person')
+  } else {
+    res.redirect('/international-arrivals/v1/travel-details-same-person-1')
+  }
+})
+
+router.post('/international-arrivals/v1/action4/travel-details-same-person-1', function (req, res) {
+  let travelDetailsSame = req.session.data['travel-details-same-person-1']
+  if (travelDetailsSame == "No") {
+    res.redirect('/international-arrivals/v1/cannot-add-person')
+  } else {
+    res.redirect('/international-arrivals/v1/people-confirmed-person-1')
+  }
+})
+
+module.exports = router
