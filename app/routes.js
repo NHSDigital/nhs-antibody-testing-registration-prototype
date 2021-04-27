bd2eb6b701d3f21367fe3ed35c461819ecc03d28
Andrea Zavodska <azavodska@deloitte.co.uk>
2021-04-27 17:42:26 +0100

    update copy

--- app/routes.js
+++ app/routes.js
@@ -2809,18 +2809,12 @@ router.post('/antibody/v7/action7/antibody-survey-or-study', function (req, res)
 router.post('/antibody/v7/action7/currently-in-work', function (req, res) {
   let currentlyInWork = req.session.data['currently-in-work']
   let antibodySurveyOrStudy = req.session.data['antibody-survey-or-study']
-<<<<<<< HEAD
-  if (currentlyInWork == "Yes"){
+  if (currentlyInWork == "Yes and I work from home" && antibodySurveyOrStudy == "Yes" || currentlyInWork == "Yes and I travel to work" && antibodySurveyOrStudy == "Yes"){
     res.redirect('/antibody/v7/refer-and-triage/area-of-work')
-  } else if (currentlyInWork == "No" && antibodySurveyOrStudy == "Yes") {
+  } else if (currentlyInWork !== "No" && antibodySurveyOrStudy == "No") {
     res.redirect('/antibody/v7/refer-and-triage/antibody-test')
   } else {
-=======
-  if (currentlyInWork == "No"){
->>>>>>> 682dd29303dba774648b04869b96c208718bf60a
     res.redirect('/antibody/v7/refer-and-triage/not-eligible-out-of-work')
-  } else {
-    res.redirect('/antibody/v7/refer-and-triage/area-of-work')
   }
 })
 
