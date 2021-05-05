32186e214567288a3098a6f64ec3bf6a6bddbe86
Richard Payne <richy_payne@hotmail.co.uk>
2021-05-05 13:45:57 +0100

    lfd 17.1 new triage iteration

--- app/routes/litereg-accounts.js
+++ app/routes/litereg-accounts.js
@@ -1017,6 +1017,41 @@ router.post('/share-result-lateral-flow/v17/action9/check-mobile', function (req
   }
   })
 
+
+  // Version 17.1 - LDF self report accounts - Who's taking the test route
+
+  router.post('/share-result-lateral-flow/v17-1/action9/whos-taking-the-test', function (req, res) {
+    let person = req.session.data['whos-taking-the-test']
+    if (person == "myself") {
+      res.redirect('/share-result-lateral-flow/v17-1/coronavirus-account')
+    } else {
+      res.redirect('/share-result-lateral-flow/v17-1/country')
+    }
+  })
+
+  // Version 17.1 - LDF self report accounts - Create password route
+
+  router.post('/share-result-lateral-flow/v17-1/action9/create-password', function (req, res) {
+  let password = req.session.data['password']
+  let confirmPassword = req.session.data['confirm-password']
+  if (password == "" || confirmPassword == "") {
+    res.redirect('/share-result-lateral-flow/v17-1/user-account/create-password-error')
+  } else {
+    res.redirect('/share-result-lateral-flow/v17-1/user-account/check-email')
+  }
+  })
+
+  // Version 17.1 - LDF self report accounts - check mobile route
+  router.post('/share-result-lateral-flow/v17-1/action9/check-mobile', function (req, res) {
+  let securityCode = req.session.data['security-code']
+  if (securityCode == "") {
+    res.redirect('/share-result-lateral-flow/v17-1/user-account/check-mobile-error')
+  } else {
+    res.redirect('/share-result-lateral-flow/v17-1/user-account/agreement')
+  }
+  })
+
+
   // Version 18 - LDF self report accounts - Who's taking the test route
 
   router.post('/share-result-lateral-flow/v18/action9/whos-taking-the-test', function (req, res) {
