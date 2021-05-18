1b48c37570cbb450e6c3a9d391eaf67c8e7e85d7
Andrea Zavodska <azavodska@deloitte.co.uk>
2021-05-18 19:09:41 +0100

    update copy

--- app/routes/antigen.js
+++ app/routes/antigen.js
@@ -888,18 +888,19 @@ router.post('/antigen/v2/action2/antibody-test-person-1', function (req, res) {
 
 // Antigen V2 - Fingerprick Test Person 1
 router.post('/antigen/v2/action2/fingerprick-test-person-1', function (req, res) {
-  let leadBookerAddress = req.session.data['delivery-address-antibody']
-  let leadBookerPostcode = req.session.data['delivery-address-postcode-manual-antibody']
+  // let leadBookerAddress = req.session.data['delivery-address-antibody']
+  // let leadBookerPostcode = req.session.data['delivery-address-postcode-manual-antibody']
   let fingerprickTest = req.session.data['fingerprick-test-person-1']
+  let wayToTest = req.session.data['way-to-test']
 
-  if (fingerprickTest == "Yes" && leadBookerAddress || fingerprickTest == "Yes" && leadBookerPostcode){
-    res.redirect('/antigen/v2/global-registration/check-your-answers-person-1')
-  }
-  else if (fingerprickTest == "Yes")  {
-    res.redirect('/antigen/v2/global-registration/delivery-postcode-person-1')
-  }
-  else {
-    res.redirect('/antigen/v2/global-registration/cannot-get-test-person-1')
+  if (fingerprickTest == "1,2" || fingerprickTest == "2" || fingerprickTest == "1"){
+    res.redirect('/antigen/v2/global-registration/delivery-address-person-1')
+  } else if (fingerprickTest == "1,None of these" || fingerprickTest == "2,None of these" || fingerprickTest == "1,2,None of these") {
+    res.redirect('/antigen/v2/global-registration/fingerprick-test-person-1-error')
+  } else if (fingerprickTest == "None of these" && wayToTest == "home testing") {
+    res.redirect('/antigen/v2/order-home-test-kit/')
+  } else  {
+    res.redirect('/antigen/v2/site-appointment-booking/find-test-site')
   }
 })
 
