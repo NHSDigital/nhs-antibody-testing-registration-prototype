9021dda657aa246785f44f813dc2d66ac82397b6
Richard Payne <richy_payne@hotmail.co.uk>
2021-09-21 14:28:38 +0100

    Update lite-registration.js

--- app/routes/lite-registration.js
+++ app/routes/lite-registration.js
@@ -641,6 +641,26 @@ router.post('/lite-registration-lateral-flow-accounts/v1/action9/landline-number
 
     })
 
+    // Version 18.2 - LFD Results sharing - Ethnic group route
+    router.post('/share-result-lateral-flow/v18-2/action6/ethnic-group', function (req, res) {
+      let ethnicGroup = req.session.data['ethnic-group']
+
+      if (ethnicGroup == "Asian or Asian British"){
+        res.redirect('/share-result-lateral-flow/v18-2/ethnic-background-asian')
+      } else if (ethnicGroup == "Black, African, Black British or Caribbean") {
+        res.redirect('/share-result-lateral-flow/v18-2/ethnic-background-black')
+      } else if (ethnicGroup == "Mixed or multiple ethnic groups") {
+        res.redirect('/share-result-lateral-flow/v18-2/ethnic-background-mixed')
+      } else if (ethnicGroup == "White") {
+        res.redirect('/share-result-lateral-flow/v18-2/ethnic-background-white')
+      } else if (ethnicGroup == "Another ethnic group") {
+        res.redirect('/share-result-lateral-flow/v18-2/ethnic-background-another')
+      } else {
+        res.redirect('/share-result-lateral-flow/v18-2/postcode-lookup')
+      }
+
+    })
+
     // Version 19 - LFD Results sharing - Ethnic group route
     router.post('/share-result-lateral-flow/v19/action6/ethnic-group', function (req, res) {
       let ethnicGroup = req.session.data['ethnic-group']
