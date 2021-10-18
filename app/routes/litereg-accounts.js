c06cf87151b9a3827212bbe9a589bb87b9a542fc
Amaani3 <amaaniahmed@deloitte.co.uk>
2021-10-18 15:48:30 +0100

    why are you taking this test

--- app/routes/litereg-accounts.js
+++ app/routes/litereg-accounts.js
@@ -527,7 +527,7 @@ router.post('/litereg-accounts/v2/action9/check-mobile', function (req, res) {
 // Version 2 - Lite Registration Accounts - test place route
 router.post('/litereg-accounts/v2/action5/test-place', function (req, res) {
   let testPlace = req.session.data['test-place']
-  if (testPlace == "At a test site, government quarantine hotel, or in accommodation as part of an event/conference") {
+  if (testPlace == "At a test site, government quarantine hotel, or in accommodation as part of an event/conference" or testPlace == "At a test site, government quarantine hotel, or in accommodation as part of an event/conference" ) {
     res.redirect('/litereg-accounts/v2/site-id')
   } else {
     res.redirect('/litereg-accounts/v2/royal-mail-barcode')
