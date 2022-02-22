5b285507b0adf71ea3bb076f3674e6cc2ea3de86
Richard Payne <richy_payne@hotmail.co.uk>
2022-02-22 14:14:42 +0000

    Digital Reader v33 for UR on choice screen

--- app/routes/litereg-accounts.js
+++ app/routes/litereg-accounts.js
@@ -2096,12 +2096,45 @@ router.post('/share-result-lateral-flow/v19-2/action9/check-mobile', function (r
           })
 
           // Version 29 - LDF self report accounts - check mobile route
-          router.post('/share-result-lateral-flow/v2/action9/check-mobile', function (req, res) {
+          router.post('/share-result-lateral-flow/v29/action9/check-mobile', function (req, res) {
           let securityCode = req.session.data['security-code']
           if (securityCode == "") {
             res.redirect('/share-result-lateral-flow/v29/user-account/check-mobile-error')
           } else {
             res.redirect('/share-result-lateral-flow/v29/user-account/agreement')
+          }
+          })
+
+          // Version 33 - LDF self report accounts - Who's taking the test route
+
+          router.post('/share-result-lateral-flow/v33/action9/whos-taking-the-test', function (req, res) {
+            let person = req.session.data['whos-taking-the-test']
+            if (person == "myself") {
+              res.redirect('/share-result-lateral-flow/v33/coronavirus-account')
+            } else {
+              res.redirect('/share-result-lateral-flow/v33/country')
+            }
+          })
+
+          // Version 29 - LDF self report accounts - Create password route
+
+          router.post('/share-result-lateral-flow/v33/action9/create-password', function (req, res) {
+          let password = req.session.data['password']
+          let confirmPassword = req.session.data['confirm-password']
+          if (password == "" || confirmPassword == "") {
+            res.redirect('/share-result-lateral-flow/v33/user-account/create-password-error')
+          } else {
+            res.redirect('/share-result-lateral-flow/v33/user-account/check-email')
+          }
+          })
+
+          // Version 29 - LDF self report accounts - check mobile route
+          router.post('/share-result-lateral-flow/v33/action9/check-mobile', function (req, res) {
+          let securityCode = req.session.data['security-code']
+          if (securityCode == "") {
+            res.redirect('/share-result-lateral-flow/v33/user-account/check-mobile-error')
+          } else {
+            res.redirect('/share-result-lateral-flow/v33/user-account/agreement')
           }
           })
 
