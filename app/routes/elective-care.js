57efb67bd82466682ca1d1b31dc77045e5fe6782
theomatthewsnhs <theo.matthews1@nhs.net>
2022-04-06 12:47:22 +0100

    TTOLDT-486: Bulk vs 1by1 radio button functional

--- app/routes/elective-care.js
+++ app/routes/elective-care.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 
-router.post('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/bulk-or-individual-v1', function (req, res) {
+router.post('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/bulk-or-1by1-v1', function (req, res) {
   let answer = req.body.cuUploadType;
 
   if (answer == 'bulk') {
@@ -10,7 +10,19 @@ router.post('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/bulk-or-i
   } else if (answer == '1by1') {
       res.redirect('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/1by1-procedure-date-v1')
     } else {
-    res.redirect('/OBT/registration/e2e-prototypes/lft-pcr-with-results-live/start-pages/how?error=empty')
+    res.redirect('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/bulk-or-1by1-v1-e1')
+    }
+});
+
+router.post('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/bulk-or-1by1-v1-e1', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/bulk-upload-v1')
+  } else if (answer == '1by1') {
+      res.redirect('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/1by1-procedure-date-v1')
+    } else {
+    res.redirect('/OBT/elective-care/feature-design/TTOLDT-486_lft_ordering/bulk-or-1by1-v1-e1')
     }
 });
 
