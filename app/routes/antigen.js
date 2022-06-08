a2338b86e4490e8f1596a9a38d3d17f9cfe539db
ls-nomensa <93996837+ls-nomensa@users.noreply.github.com>
2022-06-08 14:34:59 +0100

    Fix routing for global reg, landline phone number

--- app/routes/antigen.js
+++ app/routes/antigen.js
@@ -794,9 +794,10 @@ router.post('/IBT/antigen/e2e-prototypes/research/action3/how-will-you-get-test'
 router.post('/IBT/antigen/e2e-prototypes/research/action3/landline-number', function (req, res) {
   let emailAddress = req.session.data['email-address']
   let wayToTest = req.session.data['way-to-test']
-  if (!emailAddress && wayToTest == "home testing"){
-    res.redirect('/IBT/antigen/e2e-prototypes/research/global-registration/email-address-home')
-  } else {
+  if (!emailAddress){
+    res.redirect('/IBT/antigen/e2e-prototypes/research/global-registration/email-address')
+  }
+  else {
     res.redirect('/IBT/antigen/e2e-prototypes/research/global-registration/gender')
   }
 })
