1d58c981a11ae4666457cc7cd92e89c11d309d6b
theomatthewsnhs <theo.matthews1@nhs.net>
2022-06-22 15:17:18 +0100

    AC-687: Created more clear versions of PCR LFT work

--- app/routes/elective-care.js
+++ app/routes/elective-care.js
@@ -173,4 +173,62 @@ router.post('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v2/patient-tes
     }
 });
 
+//
+//V3
+//Dual running PCR/LFT service
+//PCR
+//
+
+router.post('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/bulk-or-1by1-v1', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/bulk-upload-v1')
+  } else if (answer == '1by1') {
+      res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/1by1-procedure-date-v1')
+    } else {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/bulk-or-1by1-v1-e1')
+    }
+});
+
+router.post('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/bulk-or-1by1-v1-e1', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/bulk-upload-v1')
+  } else if (answer == '1by1') {
+      res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/1by1-procedure-date-v1')
+    } else {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/PCR/bulk-or-1by1-v1-e1')
+    }
+});
+
+
+//LFT
+//
+
+router.post('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/bulk-or-1by1-v1', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/bulk-upload-v1')
+  } else if (answer == '1by1') {
+      res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/1by1-procedure-date-v1')
+    } else {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/bulk-or-1by1-v1-e1')
+    }
+});
+
+router.post('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/bulk-or-1by1-v1-e1', function (req, res) {
+  let answer = req.body.cuUploadType;
+
+  if (answer == 'bulk') {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/bulk-upload-v1')
+  } else if (answer == '1by1') {
+      res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/1by1-procedure-date-v1')
+    } else {
+    res.redirect('/OBT/elective-care/feature-design/AC-687_lft_and_pcr/v3/patient-testing-portal/LFT/bulk-or-1by1-v1-e1')
+    }
+});
+
 module.exports = router
